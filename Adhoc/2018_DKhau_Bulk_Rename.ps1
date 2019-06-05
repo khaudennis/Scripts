ecba4ec1a1a4437871e597afb2d1322f7387fad5
Dennis Khau <iam@denniskhau.com>
2019-06-04 20:03:38 -0400

    Added adhoc folder and contents.

---
+++ Adhoc/2018_DKhau_Bulk_Rename.ps1
@@ -0,0 +1,6 @@
+﻿$Directory = "C:\Data\db\"
+$OrigStr = ""
+$NewStr = ""
+
+Get-ChildItem $Directory -Recurse | 
+Foreach-Object { Rename-Item $_.FullName ($_.FullName -Replace "$OrigStr","$NewStr")}
---
+++ Adhoc/2018_DKhau_Unarchive_Folder.ps1
@@ -0,0 +1,13 @@
+﻿$ScriptDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+$z = "C:\Program Files\7-Zip\7z.exe"
+
+$files = Get-ChildItem $ScriptDir -r | where {$_.Extension -match "gz"}
+
+foreach ($file in $files) {
+    $source = $file.FullName
+    $destination = "./"
+    Write-Host $destination
+    $destination = "-o" + $destination
+    & "$z" x -y $source $destination
+    Remove-Item $source
+}
