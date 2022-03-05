0450777119fdba5b8ed69c720f7cd7f427e7806d
Dennis Khau <iam@denniskhau.com>
2022-03-05 08:26:35 -0500

    Changed auth token to bearer.

--- Bash/update-cloudflare.sh
+++ Bash/update-cloudflare.sh
@@ -4,11 +4,11 @@
 # Prerequisite:  curl, jq
 #   sudo apt update && sudo apt upgrade -y && sudo apt install jq curl -y
 # Save script in script directory, make it executable:
-#   chmod +x <name.sh> (update-cloudflare.sh)
+#   chmod +x update-cloudflare.sh
 # Modify zone, dnsrecords, email and key to match Cloudflare instance.
 # Add to cron:
 #   sudo crontab -u <service_user> -e
-#   */1  *  *  *  * <service_user> -f /home/<account>/update-cloudflare.sh
+#   */1  *  *  *  * <service_user> -f /home/<service_user>/update-cloudflare.sh
 
 # Cloudflare zone is the zone which holds the record
 zone=example.com
@@ -29,7 +29,7 @@ echo "Current IP is $ip"
 # get the zone id for the requested zone
 zoneid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones?name=$zone&status=active" \
   -H "X-Auth-Email: $cloudflare_auth_email" \
-  -H "X-Auth-Key: $cloudflare_auth_key" \
+  -H "Authorization: Bearer $cloudflare_auth_key" \
   -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
 
 echo "Zoneid for $zone is $zoneid"
@@ -49,7 +49,7 @@ do
   # get the dns record id
   dnsrecordid=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records?type=A&name=$dnsrecord" \
     -H "X-Auth-Email: $cloudflare_auth_email" \
-    -H "X-Auth-Key: $cloudflare_auth_key" \
+    -H "Authorization: Bearer $cloudflare_auth_key" \
     -H "Content-Type: application/json" | jq -r '{"result"}[] | .[0] | .id')
 
   echo "DNSrecordid for $dnsrecord is $dnsrecordid"
@@ -57,9 +57,8 @@ do
   # update the record
   curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zoneid/dns_records/$dnsrecordid" \
     -H "X-Auth-Email: $cloudflare_auth_email" \
-    -H "X-Auth-Key: $cloudflare_auth_key" \
+    -H "Authorization: Bearer $cloudflare_auth_key" \
     -H "Content-Type: application/json" \
     --data "{\"type\":\"A\",\"name\":\"$dnsrecord\",\"content\":\"$ip\",\"ttl\":1,\"proxied\":false}" | jq
-
 
 done
